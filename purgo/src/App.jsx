d4b26b69a4ffc98c6550896eb58a2d3b69effa71
[Musab Kamaldinov] <you@example.com>
2024-08-24 16:20:20 +0500

    fuller

--- purgo/src/App.jsx
+++ purgo/src/App.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Nav from './components/nav';
-import Home from './components/home';
+import React from "react";
+import {Routes, Route} from "react-router-dom";
+import Nav from './component/nav';
+import Company from './component/company';
+import Cards from './component/cards';
+import Footer from './component/footer';
+import Ad from './component/ad';
+import Timework from './component/timework';
 
-const App = () => {
-  return (
-    <div>
-      <Nav/>
-        <Routes>
-          <Route path='/Home' element={<Home/>}/>
-        </Routes>
-    </div>
-  );
+
+function App(props) {
+    
+
+    return (
+        <div className="bg-[#E3E3E3]">
+            <Nav/>
+            <Routes>
+                <Route path='/Nav' element={<Nav/>}/>
+                <Route path='/Company' element={<Company/>}/>
+                <Route path='/Cards' element={<Cards/>}/>
+                <Route path='/Ad' element={<Ad/>} />
+                <Route path='/Timework' element={<Timework/>} />
+                <Route path='/Footer' element={<Footer/>}/>
+            </Routes>
+
+        </div>
+    );
 }
 
 export default App;
--- purgo/src/main.jsx
+++ purgo/src/main.jsx
@@ -1,10 +1,12 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {BrowserRouter} from "react-router-dom"
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+import  {BrowserRouter} from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
 )
